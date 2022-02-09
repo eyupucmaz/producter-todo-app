f0332f8aeba0c1c0ffeedb3c7b32d6ca4a7e051f
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-02-09 20:34:33 +0300

    Created form component and styled

--- src/Components/TodoForm/TodoForm.js
+++ src/Components/TodoForm/TodoForm.js
@@ -1,9 +1,24 @@
+import { ListPlus } from "lucide-react";
+import { Col, Container, Row } from "react-bootstrap";
 import styles from "./TodoForm.module.css";
 
 const TodoForm = () => {
 	return (
 		<>
-			<h1>Todo Form</h1>
+			<Container>
+				<Row className="d-flex justify-content-center">
+					<Col sm md={10} lg={7} className={styles.form}>
+						<input
+							type="text"
+							placeholder="Add new task"
+							className={styles.form__input}
+						/>
+						<button className={styles.form__button}>
+								<ListPlus size={36} className={styles.button__icon}/>
+							</button>
+					</Col>
+				</Row>
+			</Container>
 		</>
 	);
 };
