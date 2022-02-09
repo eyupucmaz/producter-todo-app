71625005dda7dcb34e6b1ef386ee6f43045767bf
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-02-09 19:36:51 +0300

    Created Header Comp

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,15 @@
 import styles from "./Header.module.css";
 import { ListChecks } from "lucide-react";
-import { Container } from "react-bootstrap";
 
 const Header = () => {
 	return (
 		<>
-			<Container>
-				<div className={styles.header}>
-					<a href="/" className={styles.header__icon}>
-						<ListChecks size={48} color="#2d70fd" />
-					</a>
-					<h1 className={styles.header__text}>To Do List App</h1>
-				</div>
-			</Container>
+			<nav className={styles.header}>
+				<a href="/" className={styles.header__link}>
+					<ListChecks size={36} className={styles.header__link_icon}/>
+				</a>
+				<h1 className={styles.header__text}>to-do list app</h1>
+			</nav>
 		</>
 	);
 };
