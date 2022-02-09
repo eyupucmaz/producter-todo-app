4dccccb04ae27408337ee74a0a23b829b558028f
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-02-09 19:37:18 +0300

    Added form item to index file

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header/Header";
 export { default as List } from "./List/List";
 export { default as ListItem } from "./ListItem/ListItem";
+export { default as TodoForm } from "./TodoForm/TodoForm";
