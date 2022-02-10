079ccb584dc75c0a3a302ed71adce64af6d3d8c0
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-02-10 21:51:25 +0300

    Finish the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import { Header } from "./Components";
+import { Header, TodoForm, List } from "./Components";
+import { Container } from "react-bootstrap";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+	const [todos, setTodos] = useState(() => {
+		let localTodos = localStorage.getItem("todos");
+		return localTodos ? JSON.parse(localTodos) : [];
+	});
+
+	useEffect(() => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}, [todos]);
+
 	return (
 		<>
-			<Header />
+			<Header className={styles.header} />
+			<Container fluid className={styles.main}>
+				<TodoForm setTodos={setTodos} todos={todos} />
+				<List todos={todos} setTodos={setTodos} />
+			</Container>
 		</>
 	);
 }
