ac3d34b945f02c8301d93c7785e8c43d2fcc1934
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-02-10 21:50:50 +0300

    List component added

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,11 +1,32 @@
-import { ListItem } from "..";
+import { Trash2 } from "lucide-react";
+import { Col, Container, Row } from "react-bootstrap";
+import Item from "../Item/Item";
 import styles from "./List.module.css";
 
-const List = () => {
+const List = ({ todos, setTodos }) => {
+	const handleDelete = () => {
+		console.log("cliked");
+		setTodos(todos.filter((todo) => todo.isChecked !== true));
+	};
+
 	return (
 		<>
-			<h1>List</h1>
-			<ListItem/>
+			<Container>
+				<Row className="justify-content-center">
+					<Col sm md={10} lg={7} className={styles.list}>
+						<div className={styles.list__header}>Your Tasks</div>
+						{todos?.map((todo, index) => (
+							<Item key={index} todo={todo} todos={todos} setTodos={setTodos} />
+						))}
+						<div className={styles.list__footer}>
+							<p className={styles.footer__text}>clear all completed tasks</p>
+							<button className={styles.footer__button} onClick={handleDelete}>
+								<Trash2 className={styles.footer__button_icon} />
+							</button>
+						</div>
+					</Col>
+				</Row>
+			</Container>
 		</>
 	);
 };
