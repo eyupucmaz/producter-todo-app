fabc53c1dae81c0a4367b60a8749c0da320f484a
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-02-10 21:50:37 +0300

    Todoform added

--- src/Components/TodoForm/TodoForm.js
+++ src/Components/TodoForm/TodoForm.js
@@ -1,21 +1,57 @@
+import { useEffect, useState } from "react";
 import { ListPlus } from "lucide-react";
 import { Col, Container, Row } from "react-bootstrap";
 import styles from "./TodoForm.module.css";
 
-const TodoForm = () => {
+const TodoForm = ({ todos, setTodos }) => {
+	const [text, setText] = useState("");
+
+	const [todo, setTodo] = useState({
+		text: text,
+		isChecked: false,
+		id: Date.now(),
+	});
+	const handleChange = (e) => {
+		setText(e.target.value);
+	};
+
+	useEffect(() => {
+		setTodo({ text: text, isChecked: false, id: Date.now() });
+	}, [text]);
+
+	const handleClick = () => {
+		setTodos([...todos, todo]);
+		setText("");
+	};
+	const handleKeyPress = (e) => {
+		if (e.key === "Enter") {
+			handleClick();
+		}
+	};
+
 	return (
 		<>
 			<Container>
-				<Row className="d-flex justify-content-center">
-					<Col sm md={10} lg={7} className={styles.form}>
-						<input
-							type="text"
-							placeholder="Add new task"
-							className={styles.form__input}
-						/>
-						<button className={styles.form__button}>
-								<ListPlus size={36} className={styles.button__icon}/>
+				<Row className="justify-content-center">
+					<Col sm md={10} lg={7} className={styles.form__container}>
+						<div className={styles.form}>
+							<input
+								type="text"
+								placeholder="Add new task"
+								className={styles.form__input}
+								onChange={handleChange}
+								value={text}
+								required
+								onKeyPress={handleKeyPress}
+							/>
+							<button
+								type="button"
+								className={styles.form__button}
+								onClick={handleClick}
+							>
+								<ListPlus size={36} className={styles.button__icon} />
 							</button>
+						</div>
 					</Col>
 				</Row>
 			</Container>
